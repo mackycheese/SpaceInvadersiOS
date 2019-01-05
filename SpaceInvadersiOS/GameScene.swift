62c410124ab258ef26816ccebd8cc9e488282e2c
Jack Armstrong <mackycheese21@gmail.com>
2019-01-04 19:23:00 -0800

    Initial commit

---
+++ SpaceInvadersiOS/GameScene.swift
@@ -0,0 +1,25 @@
+//
+//  GameScene.swift
+//  SpaceInvadersiOS
+//
+//  Created by Jack Armstrong on 1/4/19.
+//  Copyright © 2019 Jack Armstrong. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func sceneDidLoad() {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        
+    }
+}
